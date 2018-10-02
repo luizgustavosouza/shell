ffe10f330d0e3dc2488b0404a0bd114dcfbc59cd
Luiz Gustavo Souza <luizgustavosouza@id.uff.br>
2018-10-02 11:44:49 -0300

    update vars

--- hardening/hardening_parte7.sh
+++ hardening/hardening_parte7.sh
@@ -5,12 +5,13 @@ IYellow='\033[0;93m'      # Yellow
 BBlue='\033[1;34m'        # Blue
 NC='\033[0m'       # Text Reset
 FILE=/var/log/hardening_$(hostname).log
-
-
+#Iniciando Escrita de Relatório
 touch /var/log/$FILE
+
+
 echo -e "${BBlue}Aplicando hardening no $(hostname)${NC}"  | tee -a /var/log/$FILE
 
-echo -e "${IYellow}Configurando dias para expiração de senha${NC}" | tee -a /var/log/hardening_$(hostname)
+echo -e "${IYellow}Configurando dias para expiração de senha${NC}" | tee -a /var/log/$FILE
 sed -i '/^PASS_MAX_DAYS/ c\PASS_MAX_DAYS   90' /etc/login.defs
 cat /etc/login.defs  | grep "PASS_MAX_DAYS   90"
 if [[ $? -eq 0 ]]; then
@@ -20,7 +21,7 @@ else
 fi
 
 
-echo -e "${IYellow}Configurando tamanho mínimo de senha${NC}" | tee -a /var/log/hardening_$(hostname)
+echo -e "${IYellow}Configurando tamanho mínimo de senha${NC}" | tee -a /var/log/$FILE
 sed -i '/^PASS_MIN_LEN/ c\PASS_MIN_LEN    10' /etc/login.defs
 grep PASS_MIN_LEN /etc/login.defs
 if [[ $? -eq 0 ]]; then
@@ -30,7 +31,7 @@ else
 fi
 
 
-echo -e "${IYellow}Configurando número mínimo de dias para trocar as senhas${NC}" | tee -a /var/log/hardening_$(hostname)
+echo -e "${IYellow}Configurando número mínimo de dias para trocar as senhas${NC}" | tee -a /var/log/$FILE
 sed -i '/^PASS_MIN_DAYS/ c\PASS_MIN_DAYS    0' /etc/login.defs
 grep PASS_MIN_DAYS /etc/login.defs
 if [[ $? -eq 0 ]]; then
@@ -40,7 +41,7 @@ else
 fi
 
 
-echo -e "${IYellow}Desabilitando a conta Root${NC}" | tee -a /var/log/hardening_$(hostname)
+echo -e "${IYellow}Desabilitando a conta Root${NC}" | tee -a /var/log/$FILE
 usermod -L root
 if [[ $? -eq 0 ]]; then
   echo -e "${IGreen}SUCESS${NC}" | tee -a /var/log/$FILE
@@ -49,7 +50,7 @@ else
 fi
 
 
-echo -e "${IYellow}Desabilitando contas de Sistema${NC}" | tee -a /var/log/hardening_$(hostname)
+echo -e "${IYellow}Desabilitando contas de Sistema${NC}" | tee -a /var/log/$FILE
 egrep -v "^\+" /etc/passwd | awk -F: '($1!="root" && $1!="sync" && $1!="shutdown" && $1!="halt" && $3<1000 && $7!="/sbin/nologin") {print}'
 if [[ $? -eq 0 ]]; then
   echo -e "${IGreen}SUCESS${NC}" | tee -a /var/log/$FILE
@@ -67,7 +68,7 @@ done
   echo -e "${IGreen}SUCESS${NC}" | tee -a /var/log/$FILE
 fi
 
-echo -e "${IYellow}Defina Grupo Padrão para conta root${NC}" | tee -a /var/log/hardening_$(hostname)
+echo -e "${IYellow}Defina Grupo Padrão para conta root${NC}" | tee -a /var/log/$FILE
 usermod –g 0 root
 if [[ $? -eq 0 ]]; then
   echo -e "${IGreen}SUCESS${NC}" | tee -a /var/log/$FILE
@@ -76,5 +77,5 @@ else
 fi
 
 
-echo -e "${IYellow}Travando Contas de Usuários Inativos${NC}" | tee -a /var/log/hardening_$(hostname)
+echo -e "${IYellow}Travando Contas de Usuários Inativos${NC}" | tee -a /var/log/$FILE
 useradd -D -f 35
--- hardening/hardening_services.sh
+++ hardening/hardening_services.sh
@@ -1,54 +1,53 @@
-#!bin/bash
-
+#!/bin/bash
 IRed='\033[0;91m'         # Red
 IGreen='\033[0;92m'       # Green
 IYellow='\033[0;93m'      # Yellow
 BBlue='\033[1;34m'        # Blue
 NC='\033[0m'       # Text Reset
-
+FILE=/var/log/hardening_$(hostname).log
 #Iniciando Escrita de Relatório
-touch /var/log/hardening_$(hostname)
+touch /var/log/$FILE
 
-echo -e "${BBlue}Aplicando hardening no $(hostname)${NC}"  | tee -a /var/log/hardening_$(hostname)
+echo -e "${BBlue}Aplicando hardening no $(hostname)${NC}"  | tee -a /var/log/$FILE
 
 #umask
-echo "${IYellow}Configure o daemon 'umask'...${NC}" | tee -a /var/log/hardening_$(hostname)
-echo 'umask 027' > /etc/sysconfig/init | tee -a /var/log/hardening_$(hostname)
+echo "${IYellow}Configure o daemon 'umask'...${NC}" | tee -a /var/log/$FILE
+echo 'umask 027' > /etc/sysconfig/init | tee -a /var/log/$FILE
 if [[ $? -eq 0 ]]; then
-  echo -e "${IGreen}SUCESS${NC}" | tee -a /var/log/hardening_$(hostname)
+  echo -e "${IGreen}SUCESS${NC}" | tee -a /var/log/$FILE
 else
-  echo  -e "${IRed}FAIL${NC}" | tee -a /var/log/hardening_$(hostname)
+  echo  -e "${IRed}FAIL${NC}" | tee -a /var/log/$FILE
 fi
 
 
 #X Window
-echo "${IYellow}Removendo o 'X Window System'...${NC}" | tee /var/log/hardening_$(hostname)
+echo "${IYellow}Removendo o 'X Window System'...${NC}" | tee /var/log/$FILE
 cd /etc/system/system/ ; unlink default.target ; ln –s /usr/lib/system?system/multi-user.target defaul.target
 if [[ $? -eq 0 ]]; then
-  echo -e "${IGreen}SUCESS${NC}" | tee -a /var/log/hardening_$(hostname)
+  echo -e "${IGreen}SUCESS${NC}" | tee -a /var/log/$FILE
 else
-  echo  -e "${IRed}FAIL${NC}" | tee -a /var/log/hardening_$(hostname)
+  echo  -e "${IRed}FAIL${NC}" | tee -a /var/log/$FILE
 fi
 
 
 #Removendo Pacotes Desnecessários
-echo -e "${IYellow}Removendo Pacotes...${NC}" | tee -a /var/log/hardening_$(hostname)
+echo -e "${IYellow}Removendo Pacotes...${NC}" | tee -a /var/log/$FILE
 for package in $(cat packages_remove.txt); do
-  rpm -q $package >> /var/log/hardening_$(hostname)
+  rpm -q $package >> /var/log/$FILE
   if [[ $? -eq 1 ]]; then
-    echo -e "${IGreen}Pacote Não Instalado${NC}" | tee -a /var/log/hardening_$(hostname)
+    echo -e "${IGreen}Pacote Não Instalado${NC}" | tee -a /var/log/$FILE
   else
-    yum remove $package 2>&1 | tee -a /var/log/hardening_$(hostname) ; echo -e "${IGreen}SUCESS${NC}" | tee -a /var/log/hardening_$(hostname)
+    yum remove $package 2>&1 | tee -a /var/log/hardening_$(hostname) ; echo -e "${IGreen}SUCESS${NC}" | tee -a /var/log/$FILE
   fi
 done
 
 
 #Desabilitando Serviços Desnecessários
-echo "${IYellow}Desabilitando Serviços${NC}" | tee -a /var/log/hardening_$(hostname)
+echo "${IYellow}Desabilitando Serviços${NC}" | tee -a /var/log/$FILE
 for service in $(cat services_disable.txt);do
   if [[ $? -eq 0 ]]; then
-    echo -e "${IRed}Serviço $service habilitado${NC}" | tee -a /var/log/hardening_$(hostname)
+    echo -e "${IRed}Serviço $service habilitado${NC}" | tee -a /var/log/$FILE
   else
-    systemctl disable $service ; echo -e "${IGreen}Serviço $service desabilitado${NC}" | tee -a /var/log/hardening_$(hostname)
+    systemctl disable $service ; echo -e "${IGreen}Serviço $service desabilitado${NC}" | tee -a /var/log/$FILE
   fi
 done
